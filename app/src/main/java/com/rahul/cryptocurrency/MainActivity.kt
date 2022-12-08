eb6b13314b149283af8d65c4f836f8213ff243d9
Rahul Sinha <rahul@rapido.bike>
2022-12-08 19:28:34 +0530

    A minor change in the onBackPressed() method

--- app/src/main/java/com/rahul/cryptocurrency/MainActivity.kt
+++ app/src/main/java/com/rahul/cryptocurrency/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         if (supportFragmentManager.backStackEntryCount > 1) {
             supportFragmentManager.popBackStack()
         } else {
-            super.onBackPressed()
+            finish()
         }
     }
 }
