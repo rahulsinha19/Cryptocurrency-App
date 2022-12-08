0a178ff1eb4c5dba198af478ebbc138e505cc0cb
Rahul Sinha <rahul@rapido.bike>
2022-12-08 16:48:52 +0530

    Fixing the fragment transaction flow

--- app/src/main/java/com/rahul/cryptocurrency/presentation/fragment/CoinListFragment.kt
+++ app/src/main/java/com/rahul/cryptocurrency/presentation/fragment/CoinListFragment.kt
@@ -1,5 +1,6 @@
 package com.rahul.cryptocurrency.presentation.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -42,6 +43,7 @@ class CoinListFragment : Fragment(), CoinClickListener {
         return _binding?.root
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         super.onViewCreated(view, savedInstanceState)
